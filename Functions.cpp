380b0226dc7584408a11617d92d6d9430a408144
Brian Ying-Chi Chen <brianc.boston@gmail.com>
2023-05-07 19:43:40 -0700

    CoinChange

--- Functions.cpp
+++ Functions.cpp
@@ -1969,6 +1969,20 @@ vector<int> findKMax(int arr[], size_t n, size_t k) {
 }
 
 
+// https://leetcode.com/problems/coin-change/
+int coinChange(vector<int>& coins, int amount) {
+    vector<int> dp(amount + 1, INT_MAX);
+    dp[0] = 0;
+    for (int i = 1; i < amount + 1; i++) {
+        for (auto c : coins) {
+            if (i - c >= 0 && dp[i - c] != INT_MAX)
+                dp[i] = min(dp[i], dp[i - c] + 1);
+        }
+    }
+    return (dp[amount] == INT_MAX ? -1 : dp[amount]);
+}
+
+
 class TrieNode {
 public:
     TrieNode* children[26];
